e8fb80b11ff6cf65b1554982570d65ec71294808
tr0fin0 <guitrofino@gmail.com>
2023-03-18 17:01:12 +0100

    ✨ add OMP implementation and comments

--- src/runge_kutta.cpp
+++ src/runge_kutta.cpp
@@ -1,12 +1,7 @@
 #include <iostream>
-
-#include <thread>
-#if defined(_OPENMP)
-#include <omp.h>
-#endif
-
 #include "runge_kutta.hpp"
 #include "cartesian_grid_of_speed.hpp"
+#include "omp.h"
 using namespace Geometry;
 
 Geometry::CloudOfPoints
@@ -18,23 +13,18 @@ Numeric::solve_RK4_fixed_vortices(double dt, CartesianGridOfSpeed const &t_veloc
 
     Geometry::CloudOfPoints newCloud(t_points.numberOfPoints());
     // On ne bouge que les points :
-
-    // #pragma omp parallel for reduction(+:p1,p2,p3)
-    // #pragma omp parallel for
+    #pragma omp parallel for
     for (std::size_t iPoint = 0; iPoint < t_points.numberOfPoints(); ++iPoint)
     {
         point p = t_points[iPoint];
         vector v1 = t_velocity.computeVelocityFor(p);
         point p1 = p + 0.5 * dt * v1;
-
         p1 = t_velocity.updatePosition(p1);
         vector v2 = t_velocity.computeVelocityFor(p1);
         point p2 = p + 0.5 * dt * v2;
-
         p2 = t_velocity.updatePosition(p2);
         vector v3 = t_velocity.computeVelocityFor(p2);
         point p3 = p + dt * v3;
-
         p3 = t_velocity.updatePosition(p3);
         vector v4 = t_velocity.computeVelocityFor(p3);
         newCloud[iPoint] = t_velocity.updatePosition(p + onesixth * dt * (v1 + 2. * v2 + 2. * v3 + v4));
@@ -53,26 +43,23 @@ Numeric::solve_RK4_movable_vortices(double dt, CartesianGridOfSpeed &t_velocity,
 
     Geometry::CloudOfPoints newCloud(t_points.numberOfPoints());
     // On ne bouge que les points :
-    // #pragma omp parallel for
+    #pragma omp parallel for
     for (std::size_t iPoint = 0; iPoint < t_points.numberOfPoints(); ++iPoint)
     {
         point p = t_points[iPoint];
         vector v1 = t_velocity.computeVelocityFor(p);
         point p1 = p + 0.5 * dt * v1;
         p1 = t_velocity.updatePosition(p1);
-
         vector v2 = t_velocity.computeVelocityFor(p1);
         point p2 = p + 0.5 * dt * v2;
         p2 = t_velocity.updatePosition(p2);
-
         vector v3 = t_velocity.computeVelocityFor(p2);
         point p3 = p + dt * v3;
         p3 = t_velocity.updatePosition(p3);
-
         vector v4 = t_velocity.computeVelocityFor(p3);
         newCloud[iPoint] = t_velocity.updatePosition(p + onesixth * dt * (v1 + 2. * v2 + 2. * v3 + v4));
     }
-
+    // cannot be parallezed because there are outside variables
     std::vector<point> newVortexCenter;
     newVortexCenter.reserve(t_vortices.numberOfVortices());
     for (std::size_t iVortex = 0; iVortex < t_vortices.numberOfVortices(); ++iVortex)
@@ -81,25 +68,21 @@ Numeric::solve_RK4_movable_vortices(double dt, CartesianGridOfSpeed &t_velocity,
         vector v1 = t_vortices.computeSpeed(p);
         point p1 = p + 0.5 * dt * v1;
         p1 = t_velocity.updatePosition(p1);
-
         vector v2 = t_vortices.computeSpeed(p1);
         point p2 = p + 0.5 * dt * v2;
         p2 = t_velocity.updatePosition(p2);
-
         vector v3 = t_vortices.computeSpeed(p2);
         point p3 = p + dt * v3;
         p3 = t_velocity.updatePosition(p3);
         vector v4 = t_vortices.computeSpeed(p3);
-
         newVortexCenter.emplace_back(t_velocity.updatePosition(p + onesixth * dt * (v1 + 2. * v2 + 2. * v3 + v4)));
     }
-
+    // cannot be parallezed because there are outside variables
     for (std::size_t iVortex = 0; iVortex < t_vortices.numberOfVortices(); ++iVortex)
     {
         t_vortices.setVortex(iVortex, newVortexCenter[iVortex],
                              t_vortices.getIntensity(iVortex));
     }
-
     t_velocity.updateVelocityField(t_vortices);
     return newCloud;
 }
