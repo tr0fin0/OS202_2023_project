964cce1920e72a5b27439068c284bdcf918abb14
tr0fin0 <guitrofino@gmail.com>
2023-03-18 17:08:39 +0100

    ✨ add getSizeGrid() method

--- src/cartesian_grid_of_speed.hpp
+++ src/cartesian_grid_of_speed.hpp
@@ -108,6 +108,11 @@ namespace Numeric
         CartesianGridOfSpeed& operator = ( CartesianGridOfSpeed const& ) = default;
         CartesianGridOfSpeed& operator = ( CartesianGridOfSpeed     && ) = default;
 
+
+    int getSizeGrid(){
+        return m_velocityField.size();
+    }
+
     private:
         std::size_t m_width, m_height;
         double      m_left, m_bottom;
