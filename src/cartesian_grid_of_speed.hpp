5e631a983a3f9ced88ed4c2e7b52361cf9e6f710
tr0fin0 <guitrofino@gmail.com>
2023-03-18 17:09:46 +0100

    🎨 edit code formatation

--- src/cartesian_grid_of_speed.hpp
+++ src/cartesian_grid_of_speed.hpp
@@ -10,14 +10,14 @@ namespace Numeric
 {
     /**
      * @brief Cartesian grid containing velocity field computing from vortices
-     * 
+     *
      */
     class CartesianGridOfSpeed
     {
     public:
-        using vector    = Geometry::Vector<double>;
+        using vector = Geometry::Vector<double>;
         using container = std::vector<vector>;
-        using point     = Geometry::Point<double>;
+        using point = Geometry::Point<double>;
 
         //@name Constructors and destructor
         //@{
@@ -25,31 +25,31 @@ namespace Numeric
          * @brief Default constructor.
          *
          * Build a undimensionned and uninitialized velocity field grid.
-         * 
+         *
          */
         CartesianGridOfSpeed() = default;
         /**
          * @brief Construct a new cartesian grid with uninitialized velocity field
-         * 
+         *
          * @param t_dimensions The dimension of the cartesian grid in number of cells per direction
          * @param m_origin     The origin (left bottom vertex) coordinate of the cartesian grid
          * @param t_hStep      The step in all direction for the regular grid
          */
-        CartesianGridOfSpeed( std::pair<std::size_t,std::size_t> t_dimensions, Geometry::Point<double> m_origin, 
-                              double t_hStep );
+        CartesianGridOfSpeed(std::pair<std::size_t, std::size_t> t_dimensions, Geometry::Point<double> m_origin,
+                             double t_hStep);
         /**
          * @brief Copy constructor
-         * 
+         *
          */
-        CartesianGridOfSpeed( CartesianGridOfSpeed const& ) = default;
+        CartesianGridOfSpeed(CartesianGridOfSpeed const &) = default;
         /**
          * @brief Move constructor
-         * 
+         *
          */
-        CartesianGridOfSpeed( CartesianGridOfSpeed     && ) = default;
+        CartesianGridOfSpeed(CartesianGridOfSpeed &&) = default;
         /**
          * @brief Destructor
-         * 
+         *
          */
         ~CartesianGridOfSpeed() = default;
         //@}
@@ -58,7 +58,7 @@ namespace Numeric
         //@{
         /**
          * @brief Get the Left Bottom Vertex point
-         * 
+         *
          * @return point The coordinate of the left bottom point
          */
         point getLeftBottomVertex() const
@@ -67,57 +67,57 @@ namespace Numeric
         }
         /**
          * @brief Get the Right Top Vertex point
-         * 
+         *
          * @return point The coordinate of the right top vertex point
          */
         point getRightTopVertex() const
         {
-            return point{m_left+m_width*m_step, m_bottom+m_height*m_step};
+            return point{m_left + m_width * m_step, m_bottom + m_height * m_step};
         }
         /**
          * @brief Return the dimension of the cartesian grid in number of cells per direction
-         * 
-         * @return std::pair<std::size_t,std::size_t> 
+         *
+         * @return std::pair<std::size_t,std::size_t>
          */
-        std::pair<std::size_t,std::size_t> cellGeometry() const
+        std::pair<std::size_t, std::size_t> cellGeometry() const
         {
             return {m_width, m_height};
         }
 
         /**
          * @brief Return the address of the first velocity vector of the velocityfield
-         * 
-         * @return double* Return as a double value. 
+         *
+         * @return double* Return as a double value.
          */
-        double* data() { return (double*)m_velocityField.data(); }
-        double const* data() const { return (double const*)m_velocityField.data(); }
+        double *data() { return (double *)m_velocityField.data(); }
+        double const *data() const { return (double const *)m_velocityField.data(); }
 
         double getStep() const { return m_step; }
 
-        void updateVelocityField( Simulation::Vortices const& t_vortices );
-        
-        vector getVelocity( std::size_t iCell, std::size_t jCell ) const 
+        void updateVelocityField(Simulation::Vortices const &t_vortices);
+
+        vector getVelocity(std::size_t iCell, std::size_t jCell) const
         {
-            return m_velocityField[iCell*m_width+jCell];
+            return m_velocityField[iCell * m_width + jCell];
         }
 
-        point updatePosition( point const& pt ) const;
+        point updatePosition(point const &pt) const;
 
-        vector computeVelocityFor( point const& p ) const;
+        vector computeVelocityFor(point const &p) const;
 
-        CartesianGridOfSpeed& operator = ( CartesianGridOfSpeed const& ) = default;
-        CartesianGridOfSpeed& operator = ( CartesianGridOfSpeed     && ) = default;
+        CartesianGridOfSpeed &operator=(CartesianGridOfSpeed const &) = default;
+        CartesianGridOfSpeed &operator=(CartesianGridOfSpeed &&) = default;
 
-
-    int getSizeGrid(){
-        return m_velocityField.size();
-    }
+        int getSizeGrid()
+        {
+            return m_velocityField.size();
+        }
 
     private:
         std::size_t m_width, m_height;
-        double      m_left, m_bottom;
-        double      m_step;
-        container   m_velocityField;
+        double m_left, m_bottom;
+        double m_step;
+        container m_velocityField;
     };
 }
 
